fbd170a4d1b62cfa44261df52b562052337fd8bf
hector101 <hector1014really32@gmail.com>
2017-04-10 05:54:33 +0100

    refactored the get prime function

--- getPrimes/src/getprime.js
+++ getPrimes/src/getprime.js
@@ -1,19 +1,30 @@
 function getPrimes(n){
-	if(typeof(n) === "string"){
-		return "Invalid input value";
-	}
-	if(n < 0){
-		return "Negative value is invalid";
-	}
-	if(n === 0){
+	//check if the argument passed is a number
+	if(isNaN(n)) {
+		return "invalid argument";
+	} else if(n < 2) { 
+		//if number is less than two return empty array
 		return [];
-	}
-	var result = [];
-	for(let i = 0; i <= n; i++){
-		if(n % i === 0 && n !== i){
-			result.push(i);
-       }
+	} else {
+		//if number starts from (2) return at least an array containing 2
+		var primes = [2], isNotPrime;
 
-	}
-	return result;
+	  	for (var i = 3; i < n; i++) {
+	  		/*
+	  			Array.prototype.some() tests whether
+	  			some element in the array has a divisor using `checkDivisor` function
+	  		*/
+	    	isNotPrime = primes.some(checkDivisor);
+	    	if ( !isNotPrime ) {
+	      		primes.push(i);
+	    	}
+	  	}
+	  	//check if a divisor exists for each number
+	  	function checkDivisor (j) {
+	    	return i % j === 0;
+	  	}
+	  	return primes;
 }
+}
+
+getPrimes(1);
